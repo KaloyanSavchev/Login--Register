9630b7f7a97b27a822afbe2367e8ffe5163149ee
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-04-24 17:23:25 +0300

    First Commit - Xml, Internet permission, style

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    dependencies {
+        classpath(libs.google.services)
+
+    }
+    dependencies {
+
+        //implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+        //implementation("com.google.firebase:firebase-auth")
+    }
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
